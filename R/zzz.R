853c038ef26f7b3c86104ffb7535c3b130cfee70
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2020-09-15 14:37:55 +0100

    Added car package

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,14 @@
 .onLoad <- function(libname, pkgname) {
   packageStartupMessage("\n\nWelcome to the BIO2020 package to support teaching")
   # Check if dependencies installed and if not add them
-  packages <- c("mosaic", "vegan")
+  packages <- c("mosaic", "vegan", "car")
   if (length(setdiff(packages, rownames(utils::installed.packages()))) > 0) {
     utils::install.packages(setdiff(packages, rownames(utils::installed.packages())), repos = "https://cloud.r-project.org")
   }
   suppressPackageStartupMessages(
     c(library(mosaic, quietly = TRUE, warn.conflicts = FALSE),
-    library(vegan, quietly = TRUE))
+      library(car, quietyly = TRUE),
+      library(vegan, quietly = TRUE))
   )
 }
 
