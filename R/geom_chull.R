6dd4d4547c1f9afa9f4d75f9f9042419a891b076
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2021-05-17 13:48:05 +0100

    Minimal working version of geom_chull function added with example script.

---
+++ R/geom_chull.R
@@ -0,0 +1,17 @@
+# Function for convex hull
+StatChull <- ggproto("StatChull", Stat,
+                     compute_group = function(data, scales) {
+                       data[chull(data$x, data$y), , drop = FALSE]
+                     },
+
+                     required_aes = c("x", "y")
+)
+geom_chull <- function(mapping = NULL, data = NULL, geom = "polygon",
+                       position = "identity", na.rm = FALSE, show.legend = NA,
+                       inherit.aes = TRUE, ...) {
+  layer(
+    stat = StatChull, data = data, mapping = mapping, geom = geom,
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
